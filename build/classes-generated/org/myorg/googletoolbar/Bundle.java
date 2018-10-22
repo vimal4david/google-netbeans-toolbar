5806d72087d1d035755a6701649bb94abc55315e
Vimal David Vincent Devaraj <VimalDavid.VincentDevaraj@costain.com>
2018-10-22 22:32:59 +0100

    Initial Cmmit

---
+++ build/classes-generated/org/myorg/googletoolbar/Bundle.java
@@ -0,0 +1,13 @@
+package org.myorg.googletoolbar;
+/** Localizable strings for {@link org.myorg.googletoolbar}. */
+@javax.annotation.Generated(value="org.netbeans.modules.openide.util.NbBundleProcessor")
+class Bundle {
+    /**
+     * @return <i>Google</i>
+     * @see GoogleActionListener
+     */
+    static String CTL_GoogleActionListener() {
+        return org.openide.util.NbBundle.getMessage(Bundle.class, "CTL_GoogleActionListener");
+    }
+    private Bundle() {}
+}
